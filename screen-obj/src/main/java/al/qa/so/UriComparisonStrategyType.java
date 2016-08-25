235a5a5250cd9cf3f1192b52128b281eaa4b20c7
Georgiy Ryabov <gryabov@griddynamics.com>
2016-08-25 16:38:08 +0300

    Added regexp and wildcard url comparator

---
+++ screen-obj/src/main/java/al/qa/so/UriComparisonStrategyType.java
@@ -0,0 +1,7 @@
+package al.qa.so;
+
+public enum UriComparisonStrategyType {
+    Plain,
+    Wildcard,
+    Regexp
+}
--- screen-obj/src/main/java/al/qa/so/utils/url/UriComparator.java
+++ screen-obj/src/main/java/al/qa/so/utils/url/UriComparator.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.regex.Pattern;
 
 /**
  * @author Alexey Lyanguzov.
@@ -17,12 +18,15 @@ import java.util.TreeMap;
 public class UriComparator {
     private static final Logger LOG = Utils.getLogger();
 
-    public static boolean areEquals(URI a, URI b, Class<? extends UrlComparisonStrategy> strategy){
-        return areEquals(a, b, getUrlPartsFor(strategy));
+    public static boolean areEqualsPlain(String currentUri, String screenUri,
+                                         Class<? extends UrlComparisonStrategy> strategy){
+        return areEqualsPlain(Utils.buildUri(currentUri),
+                Utils.buildUri(screenUri),
+                getUrlPartsFor(strategy));
     }
 
     @SuppressWarnings("all")
-    public static boolean areEquals(URI a, URI b, UriPart...parts){
+    private static boolean areEqualsPlain(URI a, URI b, UriPart...parts){
         Map<UriPart, String> splitA = splitUrl(a);
         Map<UriPart, String> splitB = splitUrl(b);
         LOG.trace("Will compare URI parts: {}", Arrays.asList(parts));
@@ -54,6 +58,34 @@ public class UriComparator {
         });
         return urlParts;
     }
+
+    public static boolean areEqualsWildcard(final String uri, final String expression) {
+        Pattern pattern = Pattern.compile(toRegex(expression));
+        return pattern.matcher(uri).matches();
+    }
+
+    public static boolean areEqualsRegexp(final String uri, final String expression) {
+        return Pattern.compile(expression).matcher(uri).matches();
+    }
+
+    private static String toRegex(final String expression) {
+        final StringBuilder regex = new StringBuilder(expression.length() * 2);
+        for (final char c : expression.toCharArray()) {
+            switch (c) {
+                case '?':
+                    regex.append(".?");
+                    break;
+                case '*':
+                    regex.append(".*");
+                    break;
+                default:
+                    regex.append(Pattern.quote(String.valueOf(c)));
+                    break;
+            }
+        }
+        return regex.toString();
+    }
+
 
     public interface CompareAll extends UrlComparisonStrategy{}
     public interface CompareWithoutQuery extends UrlComparisonStrategy{}
